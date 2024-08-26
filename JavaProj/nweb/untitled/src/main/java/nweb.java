988e76f0fb73abe5330aeac9d6b5ddee61dd1733
Piotr Heinzelman <01174157@pw.edu.pl>
2024-08-26 21:58:11 +0200

    add Java Proj

---
+++ JavaProj/nweb/untitled/src/main/java/nweb.java
@@ -0,0 +1,6 @@
+public class nweb {
+
+    public static void main( String[] args ) {
+        System.out.print("??");
+    }
+}
---
+++ JavaProj/nweb/untitled/src/test/java/testClass.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import javax.annotation.processing.SupportedAnnotationTypes;
+
+public class testClass {
+
+    @Test
+
+    public void myTest() {
+        System.out.println("my test");
+
+    }
+}
