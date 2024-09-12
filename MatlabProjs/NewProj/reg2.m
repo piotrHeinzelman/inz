6ebbdf0da63ec02e88f38be7405192ed4e5c4368
Piotr Heinzelman <piotr@heinzelman.pl>
2024-09-12 22:38:23 +0200

    add ML

---
+++ MatlabProjs/NewProj/reg2.m
@@ -0,0 +1,16 @@
+
+x = (0:.11:100)
+y = sin(x*.12)
+          
+start_ = datetime('now')
+          
+for i=1:99999
+    b = robustfit (y,x)
+end
+
+end_ = datetime('now')
+worktime=end_-start_
+
+
+
+ 
