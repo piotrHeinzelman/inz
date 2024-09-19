956bc3c2f6316d118021cc3e52acd8b17db83dac
Piotr Heinzelman <piotr@heinzelman.pl>
2024-09-19 20:27:24 +0200

    !

--- MixedProj/01.polyfit/main.py
+++ MixedProj/01.polyfit/main.py
@@ -1,37 +1,33 @@
-
-from numba import jit, cuda
+#from numba import jit, cuda
 
 import numpy as np
 import time
 import datetime
 
-#
 def readBinFile( fileName ):
     file = open( fileName, 'rb' )
     data = np.fromfile ( file, dtype=np.uint16 )
     file.close()
     return data
 
-@jit(target_backend='cuda')
-def calc( cycles, x,y):
-    for i in range(cycles):
-        a = np.polyfit(x, y, 1)
-        return a
+#@jit(target_backend='cuda')
+#@jit
 
 x = readBinFile('datax_uint16.bin')
 y = readBinFile('datay_uint16.bin')
-cycles = 1000
-a = calc(cycles, x, y)
 
 start = time.time()
 
-
-
+cycles = 1000
+for i in range(cycles):
+    a = np.polyfit(x, y, 1)
 
 end = time.time()
+
 
 d = end-start
 print ( ' duration:' , d , '[sek.]' )
 print ('')
 print ( ' X[',x.size,'] * ' , cycles )
 print ( ' result: ', a )
+
