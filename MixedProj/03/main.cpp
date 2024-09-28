6c3119ebd3f4f7ebadcfe033bdfaf380251ec2c8
Piotr Heinzelman <piotr@heinzelman.pl>
2024-09-28 18:12:33 +0200

    03

---
+++ MixedProj/03/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+
+
+cout << "--- C++  app start: ---\r\n";
+
+a0001=1;
+a0002=2;
+...
+
+
+//-- start
+clock_t before = clock();
+
+
+int (i=0;i<1000;i++){
+  b=a0001+a0002+a0003...;
+}
+
+
+
+
+
+b0001=a0001+a0002;
+b0002=a0003+a0004;
+
+
+
+
+clock_t duration = clock() - before;
+cout << "duration: " << (float)duration / CLOCKS_PER_SEC << " [sek.]\r\n \r\n";
+
+
+return 0;
+}
+
+
+
