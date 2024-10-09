cd296624b0fa6cec0e3b055a5f9aaf442fbb7187
Piotr Heinzelman <piotr@heinzelman.pl>
2024-10-10 01:10:28 +0200

    +

--- lost_found/robot/src/Main.java
+++ lost_found/robot/src/Main.java
@@ -6,17 +6,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-
-
-
+        
         try {
              robot = new Robot();
-
-
             Thread.sleep(1000 );
-
-            if (true) return;
-
             //getBonus();
 
             //
