30da133b6b5ae74d00b61d34228fb5bda1712e3b
Piotr Heinzelman <piotr@heinzelman.pl>
2024-11-14 23:53:55 +0100

    ...

--- MixedProj/03.MLP/main.py
+++ MixedProj/03.MLP/main.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy.random import randn
 
+
+
 from sklearn.neural_network import ( MLPClassifier )
 from sklearn.model_selection import ( train_test_split )
 
@@ -14,11 +16,11 @@ from sklearn.metrics import accuracy_score
 x1 = 1+randn(20,5)
 x2 =-2*+randn(20,5)
 x3 = 3+randn(20,5)
- x = np.concatenate((x1, x2, x3),axis=0)
- d = np.concatenate(([1]*20, [2]*20, [3]*20),axis=0)
+x = np.concatenate((x1, x2, x3),axis=0)
+d = np.concatenate(([1]*20, [2]*20, [3]*20),axis=0)
 
 
- (x_train, x_test, d_train, d_test) = train_test_split( x,d,test_size=0.2  )
+(x_train, x_test, d_train, d_test) = train_test_split( x,d,test_size=0.2  )
 
 net = MLPClassifier(hidden_layer_sizes=[5,2], activation='logistic', max_iter=300, solver='lbfgs' )
 
--- MixedProj/03.MLP/matlab.m
+++ MixedProj/03.MLP/matlab.m
@@ -3,29 +3,29 @@
 % page 89
 
 
-x1 = [   1.5377    1.6715    0.8978   -0.0891    2.4193;
-    2.8339   -0.2075    0.7586    1.0326    1.2916 ];
+%x1 = [   1.5377    1.6715    0.8978   -0.0891    2.4193;
+%    2.8339   -0.2075    0.7586    1.0326    1.2916 ];
 
 
-x2 = [  -1.1596    0.9080   -1.9771   -2.2938   -3.3617;
-   -2.8880   -1.1748   -2.2620   -2.8479   -1.5450 ];
+%x2 = [  -1.1596    0.9080   -1.9771   -2.2938   -3.3617;
+%   -2.8880   -1.1748   -2.2620   -2.8479   -1.5450 ];
 
 
-x3 = [   3.1832    1.9358    1.9333    4.0001    3.9111;
-    1.9702    4.6035    3.9337    1.3358    3.5946 ];
+%x3 = [   3.1832    1.9358    1.9333    4.0001    3.9111;
+%    1.9702    4.6035    3.9337    1.3358    3.5946 ];
 
 
- x1= 1+randn(2,5)
- x2=-2+randn(2,5)
- x3= 3+randn(2,5)
+ x1= 1+randn(20,5);
+ x2=-2+randn(20,5);
+ x3= 3+randn(20,5);
 
-  x =[x1;x2;x3]';
-  d =[ ones(2,1);
-  2*ones(2,1);
-  3*ones(2,1)]';
+  x =[x1;x2;x3]'
+  d =[ ones(20,1);
+  2*ones(20,1);
+  3*ones(20,1)]'
 
 % struktura MLP 2 warstwy ukryte
-% o 5 i 2 nauronach
+% i warstwa 5 neuronow, druga 2 naurony
 % algorytm uczacy mozna zmienic na inny
 
 net = feedforwardnet([5,2],'trainlm');
@@ -35,12 +35,14 @@ net = feedforwardnet([5,2],'trainlm');
 % d wartosci zadane (destination)
 net=train(net,x,d);
 
+y=net(x);
+
 % prezentacja struktury sieci
 
-view(net);
+%view(net);
+a=net
 
 % testowanie sieci na danych wejsciowych x
-y=net(x);
 
 n_errors = sum(abs(round(y-d)))
 perf=perform(net,d,y)
