ff69244132555b0e5d12f92784618c39e06fea6f
piotr@ <piotr@>
2025-01-04 15:43:41 -0800

    03 wyk

--- MixedProj/03.MLP/wykres.py
+++ MixedProj/03.MLP/wykres.py
@@ -10,6 +10,8 @@ pi=[None,None,None,None]
 pn=[None,None,None,None]
 m=[None,None,None,None]
 mi=[None,None,None,None]
+mg=[None,None,None,None]
+mgi=[None,None,None,None]
 j=[None,None,None,None]
 jb=[None,None,None,None]
 c=[None,None,None,None]
@@ -21,6 +23,8 @@ p[0]=0.006285429000854492
 pi[0]=0.0005295276641845703
 m[0]=2305
 mi[0]=1998
+mg[0]=419
+mgi[0]=211
 j[0]=0.00006
 c[0]=0.000000542
 
@@ -29,6 +33,8 @@ pi[1]=0.0046460628509521484
 pn[1]=0.2321
 m[1]=2294
 mi[1]=1998
+mg[1]=425
+mgi[1]=206
 j[1]=0.00007
 c[1]=0.0000386
 
@@ -37,6 +43,8 @@ p[2]=0.013457536697387695
 pi[2]=0.04746580123901367
 m[2]=2235
 mi[2]=2089
+mg[3]=419
+mgi[3]=205
 j[2]=0.005
 c[2]=0.000377
 
@@ -44,6 +52,8 @@ p[3]= 0.022266149520874023
 pi[3]= 0.4420018196105957
 m[3]=2164
 mi[3]=2089
+mg[3]=422
+mgi[3]=205
 j[3]=0.007
 c[3]=0.003765
 
@@ -55,6 +65,8 @@ plt.plot( x, pi, 'bo:', label='Python wlasna impl.', linewidth=.5 )
 plt.plot( x, pn, 'yo:', label='Python numba wlasna impl.', linewidth=.5 )
 plt.plot( x, m,  'ro-', label='Matlab windows 10, IDE', linewidth=.5 )
 plt.plot( x, mi, 'ro:', label='Matlab ArchLinux shell', linewidth=.5 )
+plt.plot( x, mg,  'ro-', label='Matlab windows 10, IDE GPU', linewidth=.5 )
+plt.plot( x, mgi, 'ro:', label='Matlab ArchLinux shell GPU', linewidth=.5 )
 plt.plot( x, j,  'go-', label='Java', linewidth=.5 )
 plt.plot( x, c,  'yo-', label='C', linewidth=.5 )
 
