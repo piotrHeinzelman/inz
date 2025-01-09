b357c5abab8d9aa0394ee4a0a8dce7f61366a243
piotr@ <piotr@>
2025-01-09 15:21:18 -0800

    addimg code

--- MixedProj/03.MLP/java03/src/pl/heinzelman/tools/Tools.java
+++ MixedProj/03.MLP/java03/src/pl/heinzelman/tools/Tools.java
@@ -1,8 +1,15 @@
 package pl.heinzelman.tools;
 
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+
+import static java.awt.image.BufferedImage.TYPE_BYTE_GRAY;
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 
 public class Tools {
 
@@ -39,13 +46,33 @@ public class Tools {
                 testY[i][ testYfile[i] ]=1.0d;
             }
 
-            // byte[] trainXfile =  loadBin( path + trainXname, 16, percent*784*600 );// offset=16 size=percent*784*600
-           // byte[] testXfile =   loadBin( path + testXname,  16, percent*784*100 );   // offset=16, size=percent*784*100
 
+            byte[] trainXfile = loadBin(path + trainXname, 16, percent * 784 * 600);// offset=16 size=percent*784*600
+            trainX=new double[percent*600][784];
+            for (int i=0;i<percent*100;i++) {
+                int off=i*784;
+                for (int j=0;j<784;j++){
+                    trainX[i][j]=Byte.toUnsignedInt( trainXfile[off+j] )/256.0; //0-1
+                    //System.out.println( trainX[i][j] );
+                }
+            }
 
+            byte[] testXfile =   loadBin( path + testXname,  16, percent*784*100 );   // offset=16, size=percent*784*100
+            testX=new double[percent*100][784];
+            for (int i=0;i<percent*100;i++) {
+                int off=i*784;
+                for (int j=0;j<784;j++){
+                    testX[i][j]=Byte.toUnsignedInt( testXfile[off+j] )/256.0;
+                }
+            }
 
-            System.out.println( testY );
-            double[] doubles = testY[0];
+            // show data:
+            for ( int i=0;i<100;i++ ) {
+                saveVectorAsImg(trainX[i], trainYfile[i] +"_key_is_" + i );
+            }
+            //System.out.println( testY[1][0] );
+            //System.out.println( trainXfile.length );
+            //double[] doubles = testY[0];
 
 
         } catch (IOException e) {
@@ -64,6 +91,35 @@ public class Tools {
         fis.read( bytesBuf, 0, len );
         return bytesBuf;
     }
+
+
+
+    public void saveVectorAsImg( double[] doubles, String nameSuffix ){
+        int width=28;
+        int height=28;
+            BufferedImage image = new BufferedImage( width , height , TYPE_BYTE_GRAY );
+
+            File file = new File("image"+nameSuffix+".png");
+            for ( int i=0; i<width; i++){
+                int off=i*width;
+                for (int j=0;j<height;j++){
+                    double aDouble = (1-doubles[off + j])*254;
+                    aDouble=aDouble*255;
+                    image.setRGB( j, i, (int) aDouble);
+                }
+            }
+
+
+        try {
+            ImageIO.write(image ,  "png", file );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
 
 
 }
