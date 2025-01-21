787f6df3a1d11309bc0ad685555264f5ce39cef1
Piotr Heinzelman <piotr@heinzelman.pl>
2025-01-21 02:21:26 +0100

    !

--- MixedProj/03.MLP/main.py
+++ MixedProj/03.MLP/main.py
@@ -1,56 +1,56 @@
+# from Python - Machine learning i deep learning ISBN: 978-83-283-7001-2
+# https://scikit-learn.org/dev/modules/generated/sklearn.neural_network.MLPClassifier.html
 
 import numpy as np
 import time
 import datetime
 import sys
-
-x_train = load_mnist ()
-  'data/t10k-images-idx3-ubyte'
-  'data/t10k-labels-idx1-ubyte'
-  'data/train-images-idx3-ubyte'
-  'data/train-labels-idx1-ubyte'
+import struct
+import sklearn.neural_network as snn
 
 
 
+def readFileX ( fileName , offset, percent, multi ):
+    file=open( fileName, 'rb' )
+    file.read( offset )
+    data=np.fromfile( fileName, np.uint8, percent*600*784, '', offset )
+    data=data.reshape(percent*100*multi, 784)
+    data=(data/16)
+    file.close()
+    return data
+
+def readFileY ( fileName , offset, percent, multi ):
+    file=open( fileName, 'rb' )
+    file.read( offset )
+    data=np.fromfile( fileName, np.uint8, percent*100*multi, '', offset )
+    file.close()
+    return data
+
+
+percent=1
+trainX = readFileX ('data/train-images-idx3-ubyte', 16, percent ,6 )
+trainY = readFileY ('data/train-labels-idx1-ubyte', 8, percent, 6 )
+testX = readFileX ('data/t10k-images-idx3-ubyte', 16, percent, 1  )
+testY = readFileY ('data/t10k-labels-idx1-ubyte', 8, percent, 1 )
+
+#print ( trainX[0] )
+#print ( trainY )
+#print ( testX[0] )
+#print ( testY )
+
+#  'data/t10k-images-idx3-ubyte'
+#  'data/t10k-labels-idx1-ubyte'
+#  'data/train-images-idx3-ubyte'
+#  'data/train-labels-idx1-ubyte'
+
+net = snn.MLPClassifier(hidden_layer_sizes=(64,64,10), activation='logistic', solver='sgd', alpha=0.0001, learning_rate='constant', learning_rate_init=0.001)
+net.fit(trainX, trainY)
+
+print (net)
 
 
 
 
 
 
-
-
-
-
-
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import (train_test_split)
-from sklearn.neural_network import (MLPClassifier)
-
-x_train, y_train = load_mnist( kind='train' )
-x_test, y_test = load_mnist( kind='t10k' )
-
-
-print( x_train.size )
-print( x_test.size )
-
-( x_train, x_trash, y_train, y_trash) = train_test_split( x_train, y_train, test_size=0.1 ) 
-( x_test, x_trash, y_test, y_trash)   = train_test_split( x_test , y_test , test_size=0.1 )
-
-print( x_train.size )
-print( x_test.size )
-
-
-
-for i in range ( 50, 750, 10 ):
-    net = MLPClassifier( hidden_layer_sizes=[32,32,10], activation='logistic', max_iter=i, solver='lbfgs' )
-    net.fit( x_train, y_train )
-    score =  accuracy_score( y_test, net.predict( x_test ))   
-    loss  =  net.loss_ 
-
-    print( '# cycles:   (%i) ' % i )
-    print( '# Accuracy score: (%f) ' % score )
-    print( '# Net loss:       (%f) ' % loss  )
-    print( 'p[]=[ %i, %f ] ' % ( i, score ) )
-#    print(i)
 
