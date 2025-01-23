cfcfd3fcb1b2faf5f9f2cbb375207322e01250b8
Piotr Heinzelman <piotr@heinzelman.pl>
2025-01-24 00:14:58 +0100

    03 Keras

--- MixedProj/03.MLP/main_scikit.py
+++ MixedProj/03.MLP/main_scikit.py
@@ -1,14 +1,13 @@
-# https://keras.io/examples/vision/mnist_convnet/
-# https://keras.io/getting_started/ 
-
+# from Python - Machine learning i deep learning ISBN: 978-83-283-7001-2
+# https://scikit-learn.org/dev/modules/generated/sklearn.neural_network.MLPClassifier.html
 
 import numpy as np
 import time
+import datetime
+import sys
+import struct
+import sklearn.neural_network as snn
 import matplotlib.pyplot as plt
-
-import keras
-from keras import layers
-
 
 
 def readFileX ( fileName , offset, percent, multi ):
@@ -17,6 +16,7 @@ def readFileX ( fileName , offset, percent, multi ):
     data=np.fromfile( fileName, np.uint8, percent*100*784*multi, '', offset )
     data=data.reshape(percent*100*multi, 784)
     data=1-(data/128)
+#   data=(data/16)
     file.close()
     return data
 
@@ -35,21 +35,27 @@ def readFileY ( fileName , offset, percent, multi ):
     return out
 
 
-percent=1
+percent=100
 trainX = readFileX ('data/train-images-idx3-ubyte', 16, percent ,6 )
 trainY = readFileY ('data/train-labels-idx1-ubyte', 8, percent, 6 )
 testX = readFileX ('data/t10k-images-idx3-ubyte', 16, percent, 1  )
 testY = readFileY ('data/t10k-labels-idx1-ubyte', 8, percent, 1 )
 
-print ( trainX[0] )
-print ( trainY )
-print ( testX[0] )
-print ( testY )
+#print ( trainX[0] )
+#print ( trainY )
+#print ( testX[0] )
+#print ( testY )
+#print(testY)
+
+#  'data/t10k-images-idx3-ubyte'
+#  'data/t10k-labels-idx1-ubyte'
+#  'data/train-images-idx3-ubyte'
+#  'data/train-labels-idx1-ubyte'
 
 
+#print(trainX[0])
 
-
-if (True):
+if (False):
     fig, ax = plt.subplots( nrows=2, ncols=5, sharex=True, sharey=True )
     ax=ax.flatten()
     img = trainX[0].reshape(28,28)
@@ -60,44 +66,23 @@ if (True):
     plt.show()
 
 
-# Model / data parameters
-num_classes = 10
-input_shape = (28, 28, 1)
 
 
+#net = snn.MLPClassifier(hidden_layer_sizes=(64,64,10), activation='logistic', solver='sgd', alpha=0.0001, learning_rate='constant', learning_rate_init=0.001, max_iter=5000 )
+net = snn.MLPClassifier(hidden_layer_sizes=(64,64), random_state=1, alpha=0.0001, max_iter=5000, early_stopping=False, activation='logistic', solver='sgd', learning_rate='constant', learning_rate_init=0.001 )
+net.fit( trainX, trainY )
 
-# model.add(layers.Dense(64, activation=activations.relu))
-# This is equivalent to:
-
-# from keras import layers
-#from keras import activations
-
-# model.add(layers.Dense(64))
-# model.add(layers.Activation(activations.relu))
-
-
-model = keras.Sequential(
-    [
-        keras.Input(shape=input_shape),
-        layers.Dense(64, activation="sigmoid" ),
-        layers.Dense(64, activation="sigmoid" ),
-        layers.Dense(num_classes, activation="softmax"),
-    ]
-)
-
-model.summary()
-
-
-# Train the model
-epochs = 500
-
-model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-
+l=[]
 
 start=time.time()
 
+
 for i in range(10):
-    model.fit(trainX, trainY, epochs=epochs, validation_split=0.1)
+    for epo in range (500):
+        net.partial_fit( trainX, trainY )
+    # l.append( net.score(testX, testY) )   
+
+# print (l)
 
 end=time.time()
 d=end-start
@@ -106,12 +91,10 @@ print("ps[]=" , d)
 
 
 
+#print (net)
+print ("# loss:", net.loss_ , ", score: " , net.score( testX, testY ), ", predict: ",net.predict(testX[:1]) )
 
-# Evaluate the trained model
-score = model.evaluate(testX, testY, verbose=0)
-print("Test loss:", score[0])
-print("Test accuracy:", score[1])
-
+#resultY = net.predict( testX )
 
 
 
