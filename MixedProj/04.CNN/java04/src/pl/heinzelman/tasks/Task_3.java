3fc7cb19e1ff57ac32c8ddd8b46367d7453b8165
piotr <piotr@i-zn.pl>
2025-04-11 09:26:21 +0200

    04

--- MixedProj/04.CNN/java04/src/pl/heinzelman/tasks/Task_3.java
+++ MixedProj/04.CNN/java04/src/pl/heinzelman/tasks/Task_3.java
@@ -5,8 +5,6 @@ import pl.heinzelman.neu.LayerSigmoidFullConn;
 import pl.heinzelman.neu.LayerSoftmaxMultiClass;
 import pl.heinzelman.tools.Tools;
 
-import java.util.Arrays;
-
 public class  Task_3 implements Task{
 
     private float[][] testX;
@@ -17,21 +15,19 @@ public class  Task_3 implements Task{
     private float[][][] testXX;
     private float[][][] trainXX;
 
-    private LayerPoolingMax layerPoolMAX = new LayerPoolingMax(1,1);
-    private LayerSigmoidFullConn layer1FC;
-    private LayerSoftmaxMultiClass layer2SoftmaxMulticlass;
-
-    private LayerFlatten layerFlatten = new LayerFlatten();
     private LayerConv layerConv = new LayerConv( 3 , 8, null, null  );
+    private LayerFlatten layerFlatten = new LayerFlatten();
+    private LayerPoolingMax layerPoolMAX = new LayerPoolingMax(1,1);
+    private LayerSigmoidFullConn layer1FC = new LayerSigmoidFullConn( 26*26*8, 10 );
+    private LayerSoftmaxMultiClass layer2SoftmaxMulticlass = new LayerSoftmaxMultiClass( 10 );
+
+    float[][][] oneX = new float[1][28][28];
+    float[][] out1x10 = new float[1][10];
+
     private Tools tools = new Tools();
 
-    private static final _Convolution conv=new _Convolution();
-    private static final _MaxPool pool=new _MaxPool();
-    private static _SoftMax softmax;
-    private float[][][] filters;
     private float ce_loss=0.0f;
     private int accuracy=0;
-    float[][] out1x10 = new float[1][10];
 
 
     public void prepare() {
@@ -52,35 +48,14 @@ public class  Task_3 implements Task{
         oneX[0] = trainXX[0];
         layerConv.setUpByX( oneX );
 
-        layer1FC = new LayerSigmoidFullConn( 26*26*8, 10 ); layer1FC.setName("Layer1"); // n neurons
-        layer2SoftmaxMulticlass = new LayerSoftmaxMultiClass( 10 ); layer2SoftmaxMulticlass.setName("Layer2Softmax"); // n neurons
-
-        softmax=new _SoftMax(13*13*filterNum,10, filterNum );
-        filters = new float[ filterNum ][3][3];
-            for (int k = 0; k < filterNum; k++) { filters[k] = _Mat.m_random(3, 3); }
     }
 
-
-
-    public float[][] _forward_( float[][] X ){
-        // perform convolution 28*28 --> 8x26x26
-        float[][][] out = conv.forward( X, filters, 8 );
-
-        // perform maximum pooling  8x26x26 --> 8x13x13
-        out = pool.forward( out );
-
-        // perform softmax operation  8*13*13 --> 10
-        float[][] out_l = softmax.forward( out );
-        return out_l;
-    }
 
     public float[][] forward_( float[][] X ){
-        float[][][] oneX = new float[1][28][28];
+
         oneX[0] = X;
         layerConv.setX( oneX );
         float[][][] Zconv = layerConv.Forward();
-
-        // System.out.println( Zconv.length + " : " + Zconv[0].length + " ! " + Zconv[0][0].length );
 
         float[] Xf = layerFlatten.Forward(Zconv);
         float[] Z1 = layer1FC.nForward( Xf );
@@ -91,34 +66,13 @@ public class  Task_3 implements Task{
         return out1x10;
     }
 
-
-    public float[] _backward_( float[][] gradient ){
-        float learn_rate=0.005f;
-        float[][][] sm_gradient=softmax.backprop( gradient,learn_rate );
-        float[][][] mp_gradient=pool.backprop( sm_gradient );
-        conv.backprop( mp_gradient, learn_rate );
-        return null;
-    }
-
     public float[] backward_( float[][] gradient ){
-        // System.out.println( gradient.length );
-        // System.out.println(Arrays.toString( gradient[0] ));
         float[] L2eOUT = layer2SoftmaxMulticlass.nBackward( gradient[0] );
         float[] eOUT = layer1FC.nBackward( L2eOUT );
         float[][][] eOUTF    = layerFlatten.Backward( eOUT );
                                layerConv.Backward( eOUTF );
-
-        // System.out.println( eOUTF.length + " : "+ eOUTF[0].length + " : "+ eOUTF[0][0].length );
         return null;
     }
-
-
-
-
-// ****************************
-
-
-
 
 
 // *********************
@@ -126,10 +80,13 @@ public class  Task_3 implements Task{
     @Override
     public void run() {
 
-        train(6000);
-        test(1000);
-/*
-        train(6000);
+        train(60000);
+        test(10000);
+
+        train(60000);
+        test(10000);
+
+        train(60000);
         test(10000);
 
         train(5000);
@@ -141,10 +98,6 @@ public class  Task_3 implements Task{
         train(5000);
         test(1000);
 
-        train(5000);
-        test(1000);
-
- */
     }
 
     public void train( int training_size ){
@@ -216,7 +169,7 @@ public class  Task_3 implements Task{
             float[] trueZ = trainY[ind_ex];
             int correct_label = tools.getIndexMaxFloat( trueZ );
 
-            out_l = _forward_(X);
+            out_l = forward_(X);
 
             // compute cross-entropy loss
             int findClass = tools.getIndexMaxFloat( out_l[0] );
@@ -227,7 +180,6 @@ public class  Task_3 implements Task{
             sum ++;
         }
         System.out.println(" ** TEST ** "+ (100*accuracy)/sum + "%,  errors: "+ ( sum-accuracy ) + "\n" );
-        // TT.printTable2( errors );
     }
 
 }
