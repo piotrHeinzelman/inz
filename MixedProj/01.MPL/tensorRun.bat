846a0026aad6cd833f9cbce3e3e9771a67b7e286
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2025-07-14 15:09:12 -0700

    Tensor run

---
+++ MixedProj/01.MPL/tensorRun.bat
@@ -0,0 +1,15 @@
+
+call c:\Users\john\tf\Scripts\activate
+cd X:\inz\MixedProj\01.MPL
+python Python.Tensorflow\main_tensor.py >> log\tensor.log
+pause
+exit
+
+
+echo python -m venv c:\Users\john\tf
+echo c:\Users\john\tf\Scripts\activate
+
+REM # Anything above 2.10 is not supported on the GPU on Windows Native
+echo python -m pip install "tensorflow<2.11"
+REM 
+echo python -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
