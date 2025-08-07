d659aa6c5727e4d9617750a57b179066d724f460
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2025-08-08 00:14:27 +0200

    Tensorflow 3

--- MixedProj/01.MPL/Python.Tensorflow/main_tensor.py
+++ MixedProj/01.MPL/Python.Tensorflow/main_tensor.py
@@ -2,15 +2,24 @@
 # https://www.tensorflow.org/?hl=pl
 # https://www.osc.edu/resources/getting_started/howto/howto_use_gpu_in_python
 # https://docs.conda.io/projects/conda/en/latest/user-guide/getting-started.html
+# https://www.tensorflow.org/api_docs/python/tf/keras/Sequential
+# https://devdocs.io/tensorflow~2.4/keras/activations
 
 # tensorflow==2.14.0  ===>>>  cuda-12.* (cuda-12.1, cuda-12.2)
+# https://keras.io/getting_started/
+
 
 import tensorflow as tf
 
-import numpy as np
-import time
-from tensorflow.keras.backend import clear_session
+#import numpy as np
+#import time
+#from tensorflow.keras.backend import clear_session
+#from tensorflow import keras
 
+
+import numpy as np
+import keras
+from keras import layers
 
 
 physical_devices = tf.config.list_physical_devices('GPU')
@@ -21,8 +30,6 @@ if physical_devices:
 
 device_name = tf.test.gpu_device_name()
 print(device_name)
-
-
 
 # params
 epochs = 50 #  5000
@@ -62,13 +69,96 @@ testX = testX.reshape(1*percent*100, 784).astype("float32") / 255
 
 
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Input(shape=(784,)),
-    tf.keras.layers.Dense(64, activation='sigmoid'),
-    tf.keras.layers.Dense(64, activation='sigmoid'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10, activation='softmax')
-])
+# Model / data parameters
+num_classes = 10
+input_shape = (28, 28, 1)
+
+# Load the data and split it between train and test sets
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# Scale images to the [0, 1] range
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+# Make sure images have shape (28, 28, 1)
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print("x_train shape:", x_train.shape)
+print(x_train.shape[0], "train samples")
+print(x_test.shape[0], "test samples")
+
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+# Model / data parameters
+num_classes = 10
+input_shape = (28, 28, 1)
+
+# Load the data and split it between train and test sets
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# Scale images to the [0, 1] range
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+# Make sure images have shape (28, 28, 1)
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print("x_train shape:", x_train.shape)
+print(x_train.shape[0], "train samples")
+print(x_test.shape[0], "test samples")
+
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+
+
+
+
+
+model = keras.Sequential(
+    [
+        keras.Input(shape=input_shape),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(num_classes, activation="softmax"),
+    ]
+)
+
+model.summary()
+
+
+
+
+
+
+
+
+
+batch_size = 128
+epochs = 15
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+
+
+#model = tf.keras.models.Sequential([
+#    tf.keras.layers.Input(shape=(784,)),
+#    tf.keras.layers.Dense(64, activation='sigmoid'),
+#    tf.keras.layers.Dense(64, activation='sigmoid'),
+#    tf.keras.layers.Dropout(0.2),
+#    tf.keras.layers.Dense(10, activation='softmax')
+#])
 
 
 exit()
