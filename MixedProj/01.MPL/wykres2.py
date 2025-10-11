5e39822c97cf3eaf6c65964755288ef9392fb6f0
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2025-10-11 22:36:30 +0200

    !

--- MixedProj/01.MPL/wykres2.py
+++ MixedProj/01.MPL/wykres2.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.title('Trening klasyfikatora MLP zależność czas/dokładność')
+plt.title('Trening klasyfikatora MLP zależność czas/dokładność Matlab')
 plt.style.use('_mpl-gallery')
 
 
