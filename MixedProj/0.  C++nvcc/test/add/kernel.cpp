eb3eb16b7950645d05aefcb7f247a0fa009319e1
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2025-10-13 00:14:55 +0200

    git add .

---
+++ MixedProj/0.  C++nvcc/test/add/kernel.cpp
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <stdint.h>
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int const IMGSIZE =1024;
+
+    //printf( "#  --- C++ ---\n+lenx:%i", lenx);
+    srand((int)time(0));
+
+    //testLoadAndSave();
+    float* C = new float [IMGSIZE];
+    for (int i = 0; i < IMGSIZE; i++) {
+        C[i] = 0.001f*i;
+    }
+
+    float D = 0.0f;
+    printf("value: %f, %f\r\n", C[1], C[1023]);
+
+
+
+
+    //-- start
+    clock_t before = clock();
+
+    for (int i = 0; i < IMGSIZE; i++) {
+	D+=C[i];
+    }
+
+    clock_t duration = clock() - before;
+    printf("\r\nduration: %lu [clocks tick], %ld[sek]\r\n", duration, duration/CLOCKS_PER_SEC );
+
+    printf("D:%f \n", D);
+
+}
