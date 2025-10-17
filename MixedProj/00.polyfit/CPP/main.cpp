05a234fdb736589ba41e0b033c2b3b49338e1cdf
piotr <piotr@i-zn.pl>
2025-10-17 16:21:36 +0200

    !

--- MixedProj/00.polyfit/CPP/main.cpp
+++ MixedProj/00.polyfit/CPP/main.cpp
@@ -10,15 +10,22 @@ using namespace std;
 
 
 int main() {
+
+    cout << "start" <<"\n";
    long len = 64000000;
    clock_t tableStart = clock();
-   double X[ len ];
-   double Y[ len ];
+   double* X=new double[len];
+   double* Y=new double[len];
    for ( int i=0; i<len; i++ ){
       X[i]=0.1*i;
       Y[i]=0.2*i;
    }
+   
    clock_t tableEnd = clock();
+   
+   clock_t durationTable = tableEnd - tableStart;
+   
+   cout << "table loaded: " <<  durationTable/(CLOCKS_PER_SEC/1000)  << "[msek.]\n";
 
 //-- start
   clock_t before = clock();
@@ -44,9 +51,10 @@ int main() {
       w0 = ysr -(w1 * xsr) ;
 
   clock_t duration = clock() - before;
-  clock_t durationTable = tableEnd - tableStart;
+  
 
-  cout << "create table time: " << (float) duration / CLOCKS_PER_SEC << " [sek.],  w0: "<<"\n";
+  cout << "create table time: " << (float) durationTable / CLOCKS_PER_SEC << " [sek.],  w0: "<<"\n";
   cout << "#  time: " << (float)duration / CLOCKS_PER_SEC << " [sek.]"  << "\n";
   return 0;
+ 
 }
