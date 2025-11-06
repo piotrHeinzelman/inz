a90c08e8bb3f3a2570d12b62fa334efd86d9bba4
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2025-11-06 01:12:21 +0100

    !

--- MixedProj/02.CNN.mnist/Python/tensorflow.py
+++ MixedProj/02.CNN.mnist/Python/tensorflow.py
@@ -1,18 +1,21 @@
-# https://keras.io/examples/vision/mnist_convnet
-# https://www.tensorflow.org/?hl=pl
-# https://www.osc.edu/resources/getting_started/howto/howto_use_gpu_in_python
-# https://docs.conda.io/projects/conda/en/latest/user-guide/getting-started.html
-
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow import config
+
 import numpy as np
 import time
-from tensorflow.keras.backend import clear_session
-from tensorflow.keras.utils import to_categorical
 
-#physical_devices = tf.config.list_physical_devices('GPU')
-#if physical_devices:
-#   for gpu in physical_devices:
-#      tf.config.experimental.set_memory_growth(gpu, True)
+
+physical_devices = config.list_physical_devices('GPU')
+if physical_devices:
+   for gpu in physical_devices:
+      config.experimental.set_memory_growth(gpu, True)
+      print(gpu.device_type)
+
+device_name = tf.test.gpu_device_name()
+print(device_name)
+
+#exit()
 
 # params
 epochs = 20
@@ -43,12 +46,11 @@ def AlexNet():
    NUMBER_OF_CLASSES = 10
    return keras.models.Sequential([
       keras.layers.Input(shape=( 28, 28, 1 )),
-#      keras.layers.Conv2D(name='conv1', filters=20, kernel_size=(5,5), activation='relu', input_shape=( 28, 28, 1 )),
       keras.layers.Conv2D(name='conv1', filters=20, kernel_size=(5,5), activation='relu' ),
       keras.layers.BatchNormalization(),
       keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2)),
       keras.layers.Flatten(),
-      keras.layers.Dense(64, activation='relu'),
+      keras.layers.Dense(64, activation='sigmoid'),
 #      keras.layers.Dense(64, activation='relu'),
 #      keras.layers.Dropout(0.2),
       keras.layers.Dense(10, activation='softmax')
@@ -58,27 +60,23 @@ def AlexNet():
 
 start=time.time()
 
-trainX = readFileX ('../../01.MLP/data/train-images-idx3-ubyte', 16, percent ,6 )
-trainY = readFileY ('../../01.MLP/data/train-labels-idx1-ubyte', 8, percent, 6 )
-testX = readFileX ( '../../01.MLP/data/t10k-images-idx3-ubyte', 16, percent, 1  )
-testY = readFileY ( '../../01.MLP/data/t10k-labels-idx1-ubyte', 8, percent, 1 )
+trainX = readFileX ('../../01.MPL/data/train-images-idx3-ubyte', 16, percent ,6 )
+trainY = readFileY ('../../01.MPL/data/train-labels-idx1-ubyte', 8, percent, 6 )
+testX = readFileX ( '../../01.MPL/data/t10k-images-idx3-ubyte', 16, percent, 1  )
+testY = readFileY ( '../../01.MPL/data/t10k-labels-idx1-ubyte', 8, percent, 1 )
 
 
 trainX = trainX.astype("float32") # / 255
 testX = testX.astype("float32") # / 255
 
-# trainY = trainY.astype("float32") # / 255
 trainY = trainY.astype("int") # / 255
-# testY = testY.astype("float32") # / 255
 testY = testY.astype("int") # / 255
 
 
-#trainX = trainX.reshape(6*percent*100, 784).astype("float32") / 255
 trainX = trainX.reshape(6*percent*100, 28,28).astype("float32") / 255
-#testX = testX.reshape(1*percent*100, 784).astype("float32") / 255
 testX = testX.reshape(1*percent*100, 28,28).astype("float32") / 255
 
-end=time.time() 
+end=time.time()
 loadTime=end-start
 
 
@@ -125,7 +123,7 @@ with tf.device('/device:GPU:0'):
 end=time.time() 
 trainTime=end-start
 
-clear_session()
+tf.keras.clear_session()
 
 
 start=time.time()
