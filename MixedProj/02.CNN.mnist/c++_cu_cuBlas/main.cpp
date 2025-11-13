5dd7db3938766df43ebf7d6a8e418421cd9b7653
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2025-11-13 01:59:44 +0100

    !

--- MixedProj/02.CNN.mnist/c++_cu_cuBlas/main.cpp
+++ MixedProj/02.CNN.mnist/c++_cu_cuBlas/main.cpp
@@ -1,4 +1,9 @@
 // https://forums.developer.nvidia.com/t/how-use-the-cudnn-graph-api-for-do-a-convolution/321806
+// https://hpc.pku.edu.cn/docs/20170830182053891231.pdf
+// https://www.youtube.com/watch?v=GPBeiKYkuZE
+// https://docs.nvidia.com/deeplearning/cudnn/archives/cudnn-892/pdf/cuDNN-Developer-Guide.pdf
+// https://docs.nvidia.com/deeplearning/cudnn/backend/v9.15.0/api/cudnn-cnn-library.html
+// https://docs.nvidia.com/deeplearning/cudnn/backend/v9.15.0/api/cudnn-graph-library.html#cudnncreate
 //
 // run:
 // nvcc -lcuda -lcublas -lcudnn *.cu -o CNN
@@ -16,7 +21,6 @@
 #include <cublas.h>
 #include <cuda.h>
 #include "tools.cu"
-#include "tools_cuBlas.cu"
 //using namespace std;
 
 
@@ -42,11 +46,66 @@ int main() {
    delete Y;
    }
 
-   int device = initDevice();
-   cudnnHandle_t handle = createHandle();
-//    cudnnHandle_t handle_;
-//    cudnnCreate(&handle_);
-//    std::cout << "Created cuDNN handle" << std::endl;
+
+   if (false){ // *************
+
+    cudaSetDevice(0); // use GPU0
+    int device;
+    struct cudaDeviceProp devProp;
+    cudaGetDevice(&device);
+    cudaGetDeviceProperties(&devProp, device);
+    std::cout << "Compute capability:" << devProp.major << "." << devProp.minor << std::endl;
+
+    cudnnHandle_t handle_;
+    cudnnCreate(&handle_);
+    std::cout << "Created cuDNN handle" << std::endl;
+
+    // create the tensor descriptor
+    cudnnDataType_t dtype = CUDNN_DATA_FLOAT;
+    cudnnTensorFormat_t format = CUDNN_TENSOR_NCHW;
+    int n = 1, c = 1, h = 1, w = 10;
+    int NUM_ELEMENTS = n*c*h*w;
+    cudnnTensorDescriptor_t x_desc;
+    cudnnCreateTensorDescriptor(&x_desc);
+    cudnnSetTensor4dDescriptor(x_desc, format, dtype, n, c, h, w);
+
+    // create the tensor
+    float *x;
+    cudaMallocManaged(&x, NUM_ELEMENTS * sizeof(float));
+    for(int i=0;i<NUM_ELEMENTS;i++) x[i] = i * 1.00f;
+    std::cout << "Original array: ";
+    for(int i=0;i<NUM_ELEMENTS;i++) std::cout << x[i] << " ";
+
+    // create activation function descriptor
+    float alpha[1] = {1};
+    float beta[1] = {0.0};
+    cudnnActivationDescriptor_t sigmoid_activation;
+    cudnnActivationMode_t mode = CUDNN_ACTIVATION_SIGMOID;
+    cudnnNanPropagation_t prop = CUDNN_NOT_PROPAGATE_NAN;
+    cudnnCreateActivationDescriptor(&sigmoid_activation);
+    cudnnSetActivationDescriptor(sigmoid_activation, mode, prop, 0.0f);
+
+    cudnnActivationForward(
+        handle_,
+        sigmoid_activation,
+        alpha,
+        x_desc,
+        x,
+        beta,
+        x_desc,
+        x
+    );
+
+    cudnnDestroy(handle_);
+    std::cout << std::endl << "Destroyed cuDNN handle." << std::endl;
+    std::cout << "New array: ";
+    for(int i=0;i<NUM_ELEMENTS;i++) std::cout << x[i] << " ";
+    std::cout << std::endl;
+    cudaFree(x);
+//    return 0;
+
+   }  // ***********
+
 
    return 0;
 /*
